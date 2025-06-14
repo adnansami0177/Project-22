4852732474de896858b61a19f01efe806174139f
unknown <you@example.com>
2025-06-14 20:11:10 +0600

    JavaScript Intro

---
+++ script.js
@@ -0,0 +1,16 @@
+let Totalmoney = 1000;
+
+let Totalegg = 5;
+let eacheggprice = 11;
+
+let Totalchocolate = 2;
+let eachchocolate = 40;
+
+let eggprice = Totalegg * eacheggprice
+let chocolateprice = Totalchocolate * eachchocolate
+
+let Totalcost = eggprice + chocolateprice
+
+let reamingmoney = Totalmoney - Totalcost
+
+console.log("Reaming Money = "+reamingmoney)
